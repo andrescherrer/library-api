96ff5041c6b3954743a3c52cee6e8556ca311154
Andre C. Scherrer <andrescherrer@gmail.com>
2024-05-13 02:21:41 -0300

    feat: add route /loans

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\V1\AuthorController;
 use App\Http\Controllers\API\V1\BookController;
+use App\Http\Controllers\API\V1\LoanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,4 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('/authors', AuthorController::class);
 Route::apiResource('/books', BookController::class);
+Route::apiResource('/loans', LoanController::class);
