2e45f2f0cdb6d511ee6a823271991dee57dd8cb9
Andre C. Scherrer <andrescherrer@gmail.com>
2024-05-13 01:05:02 -0300

    feat: define rules for StoreBookRequest to validate input data

--- app/Http/Requests/Book/StoreBookRequest.php
+++ app/Http/Requests/Book/StoreBookRequest.php
@@ -22,7 +22,24 @@ class StoreBookRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|min:3|max:255',
+            'published_date' =>'required|date|date_format:Y-m-d',
+            'list_of_authors' =>'required|array',
+            'list_of_authors.*' =>'exists:authors,id'
+        ];        
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'O campo título é obrigatório',
+            'title.min' => 'O tamanho mínimo para o campo título é de 3 caracteres',
+            'title.max' => 'O tamanho máximo para o campo título é de 255 caracteres',
+            'published_date.required' => 'O campo data de publicação é obrigatório',
+            'published_date.date' => 'O campo data de publicação deve ser uma data válida',
+            'published_date.date_format' => 'O campo data de publicação deve estar no formato AAAA-MM-DD',
+            'list_of_authors.required' => 'O campo lista de autores é obrigatório',
+            'list_of_authors.*.exists' => 'Os autores devem existir na base de dados'
         ];
     }
 }
