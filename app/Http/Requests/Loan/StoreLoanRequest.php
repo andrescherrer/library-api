3cc26c4a045b02948ba61a876e3dc69053dc2e26
Andre C. Scherrer <andrescherrer@gmail.com>
2024-05-13 09:02:10 -0300

    feat: add rules for StoreLoanRequest to validate input data

--- app/Http/Requests/Loan/StoreLoanRequest.php
+++ app/Http/Requests/Loan/StoreLoanRequest.php
@@ -22,7 +22,27 @@ class StoreLoanRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'user_id' => 'required|exists:users,id',
+            'book_id' => 'required|exists:books,id',
+            'loan_date' => 'required|date|date_format:Y-m-d',
+            'return_date' => 'required|date|date_format:Y-m-d'
         ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'user_id.required' => 'O campo user_id é obrigatório',
+            'user_id.exists' => 'O id do usuário deve existir na base de dados',
+            'book_id.required' => 'O campo book_id é obrigatório',
+            'book_id.exists' => 'O id do livro deve existir na base de dados',
+            'loan_date.required' => 'O campo data de empréstimo é obrigatório',
+            'loan_date.date' => 'O campo data de empréstimo deve ser uma data válida',
+            'loan_date.date_format' => 'O campo data de empréstimo deve estar no formato AAAA-MM-DD',
+            'return_date.required' => 'O campo data de devolução é obrigatório',
+            'return_date.date' => 'O campo data de devolução deve ser uma data válida',
+            'return_date.date_format' => 'O campo data de devolução deve estar no formato AAAA-MM-DD',
+        ];
+        
     }
 }
